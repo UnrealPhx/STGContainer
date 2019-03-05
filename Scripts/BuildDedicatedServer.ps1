eaf78ac5890d14a7dbacf21ef981692ed45cba3e
Justin Walsh <contact.me@thejustinwalsh.com>
2019-03-04 23:41:34 -0700

    Update BuildDedicatedServer.ps1
    Looking up engine association from the uproject and attempt to find in the registry.
    Falling back to a relative path

--- Scripts/BuildDedicatedServer.ps1
+++ Scripts/BuildDedicatedServer.ps1
@@ -1,6 +1,17 @@
 $script_path = split-path -parent $MyInvocation.MyCommand.Definition
-$engine_path = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\EpicGames\Unreal Engine\4.21' -Name InstalledDirectory).InstalledDirectory
-$uat = $engine_path + '\Engine\Build\BatchFiles\RunUAT.bat'
 $project = $script_path + '\..\STGContainer.uproject'
+
+$engine_key = (Get-Content $project | Out-String | ConvertFrom-Json).EngineAssociation
+
+$engine_path = $script_path + '\..\Engine'
+if ($engine_key -match '\.') {
+    $engine_path = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\EpicGames\Unreal Engine\' + $engine_key -Name InstalledDirectory).InstalledDirectory
+}
+else {
+    $engine_path = (Get-ItemProperty -Path 'HKCU:\Software\Epic Games\Unreal Engine\Builds' -Name $engine_key).$engine_key
+}
+
+$uat = $engine_path + '\Engine\Build\BatchFiles\RunUAT.bat'
+
 
 iex "$uat BuildCookRun -project='$project' -platform=Win64 -clientconfig=Development -noclient -server -serverplatform=Linux -serverconfig=Shipping -cook -allmaps -build -stage -pak -archive"
